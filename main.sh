0ae3e1467c00a59d3d5dff074567c51489dffc90
rho <rho.rhoit@gmail.com>
2014-10-23 14:48:01 +0545

    Adding Credits
    * replace \t with 4 spaces
    * refactoring

--- main.sh
+++ main.sh
@@ -37,40 +37,40 @@ function get_color { # arg($1:color_code)
 function rotate_field { # arg($1:day, $2:group)
     f=$(($1-$2))
     if [ $f -le 0 ]; then
-		echo $((7+$f))
+        echo $((7+$f))
     else
-		echo $f
+        echo $f
     fi
 }
 
 function range_check { # arg($1:check_value)
-	if [[ $1 -gt 0 ]] && [[ $1 -le 7 ]]; then
-		echo -n # do nothing
-	else
-		Usage
-		exit 1;
-	fi
+    if [[ $1 -gt 0 ]] && [[ $1 -le 7 ]]; then
+        echo -n # do nothing
+    else
+        Usage
+        exit 1;
+    fi
 }
 
 function week_view { # arg($1:group)
     day=(Sun Mon Tue Wed Thr Fri Sat)
 
     for((i=0;i<7;i++)) {
-		field=$(rotate_field $i $1)
-		f0=$((field-1))
-		f1=$((f0+7))
+        field=$(rotate_field $i $1)
+        f0=$((field-1))
+        f1=$((f0+7))
 
-		if [ $today == $i ]; then
-			color=$(get_color 1 32)
-			cdef=$(get_color 0 0)
-		else
-			color=""
-			cdef=""
-		fi
+        if [ $today == $i ]; then
+            color=$(get_color 1 32)
+            cdef=$(get_color 0 0)
+        else
+            color=""
+            cdef=""
+        fi
 
-		echo -e ${color}${day[$i]} # $field
-		echo -e "\t${data[f0]}"
-		echo -e "\t${data[f1]}$cdef"
+        echo -e ${color}${day[$i]} # $field
+        echo -e "\t${data[f0]}"
+        echo -e "\t${data[f1]}$cdef"
     }
 }
 
@@ -78,19 +78,19 @@ function xml_dump {
     day=(sunday monday tuesday wednesday thursday friday saturday)
     echo -e "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<routine>"
     for((g=1;g<=7;g++)) {
-		echo -e "    <group name=\"$g\">"
-		grp=$(($g-2))
-		for((i=0;i<7;i++)) {
-			field=$(rotate_field $i $grp)
-			f0=$((field-1))
-			f1=$((f0+7))
+        echo -e "    <group name=\"$g\">"
+        grp=$(($g-2))
+        for((i=0;i<7;i++)) {
+            field=$(rotate_field $i $grp)
+            f0=$((field-1))
+            f1=$((f0+7))
 
-			echo "      <day name=\"${day[$i]}\">"
-			echo "        <item>${data[f0]}</item>"
-			echo "        <item>${data[f1]}</item>"
-			echo "      </day>"
-		}
-		echo -e "    </group>"
+            echo "      <day name=\"${day[$i]}\">"
+            echo "        <item>${data[f0]}</item>"
+            echo "        <item>${data[f1]}</item>"
+            echo "      </day>"
+        }
+        echo -e "    </group>"
     }
     echo "</routine>"
 }
@@ -106,31 +106,31 @@ function all_sch { # arg($1:group)
     echo -en "          $h1"
 
     for day in Sun Mon Tue Wed Thr Fri Sat ; do
-		printf "   %-7s" $day
+        printf "   %-7s" $day
     done
     echo -en "$cdef\n"
 
     today=(`date +%w`)
     for((g=1;g<=7;g++)) {
-		echo -en "$h1 Group $g: $cdef"
-		grp=$(($g-2))
-		line2=""
-		if [ "$1" == $grp ]; then c2=$(get_color 0 34);
-		else c2=""; fi
+        echo -en "$h1 Group $g: $cdef"
+        grp=$(($g-2))
+        line2=""
+        if [ "$1" == $grp ]; then c2=$(get_color 0 34);
+        else c2=""; fi
 
-		for((i=0;i<7;i++)) {
-			field=$(rotate_field $i $grp)
-			f0=$((field-1))
-			f1=$((f0+7))
-			if [ $today == $i ]; then
-				echo -en "$c1${data[$f0]}$cdef "
-				line2+=$(echo -en "$c1${data[f1]}$cdef ")
-			else
-				echo -en "$c2${data[f0]} "
-				line2+=$(echo -en "$c2${data[f1]} ")
-			fi
-		}
-		echo -e "$cdef\n          $line2"
+        for((i=0;i<7;i++)) {
+            field=$(rotate_field $i $grp)
+            f0=$((field-1))
+            f1=$((f0+7))
+            if [ $today == $i ]; then
+                echo -en "$c1${data[$f0]}$cdef "
+                line2+=$(echo -en "$c1${data[f1]}$cdef ")
+            else
+                echo -en "$c2${data[f0]} "
+                line2+=$(echo -en "$c2${data[f1]} ")
+            fi
+        }
+        echo -e "$cdef\n          $line2"
     }
 }
 
@@ -144,10 +144,10 @@ function today_view { # arg($1:group)
 function update {
     local FILE="/tmp/nea.pdf"
     if [ ! -e $FILE ]; then
-		download
+        download
     fi
     if [ -e $FILE ]; then
-		extract
+        extract
     fi
 }
 
@@ -161,19 +161,27 @@ if [ $# -eq 0 ]; then
     exit 0;
 fi
 
+function Credits {
+    h1=$(get_color 1 32)
+    cdef=$(get_color 0 0)
+    sed 's/^#.*/'`echo -e $h1`'&'`echo -e $cdef`'/' $WD/AUTHORS
+    echo
+}
+
 function Usage {
     echo -e "Usage:  batti [OPTIONS] [GROUP_NO]";
-    echo -e "\t-a | --all\tShow All"
+    echo -e "\t-a | --all\tShow All [force]"
     echo -e "\t-g | --group\tGroup number 1-7"
     echo -e "\t-t | --today\tShow today's schedule [uses with group no]"
     echo -e "\t-w | --week\tShow week's schedule"
     echo -e "\t-u | --update\tCheck for update [ignores extra options]"
     echo -e "\t-x | --xml\tDump to xml"
+    echo -e "\t-c | --credits\tDisplay the Credits"
     echo -e "\t-h | --help\tDisplay this message"
 }
 
-TEMP=$(getopt -o g:awtuxh\
-              -l all,group:,week,today,update,xml,help\
+TEMP=$(getopt -o g:awtuxch\
+              -l all,group:,week,today,update,xml,credits,help\
               -n "batti"\
               -- "$@")
 
@@ -183,15 +191,16 @@ eval set -- "$TEMP"
 
 dis=0
 while true; do
-	case $1 in
-		-a|--all)        dis=1; shift;;
-		-g|--group)      grp=$2; shift 2;;
-		-w|--week)       dis=0; shift;;
-		-t|--today)      dis=3; shift;;
-		-u|--update)     update; exit;;
-		-x|--xml)        xml_dump; exit;;
-		-h|--help)       Usage; exit;;
-		--)              shift; break;;
+    case $1 in
+        -a|--all)        dis=1; shift;;
+        -g|--group)      grp=$2; shift 2;;
+        -w|--week)       dis=0; shift;;
+        -t|--today)      dis=3; shift;;
+        -u|--update)     update; exit;;
+        -x|--xml)        xml_dump; exit;;
+        -c|--credits)    Credits; exit;;
+        -h|--help)       Usage; exit;;
+        --)              shift; break;;
     esac
 done
 
